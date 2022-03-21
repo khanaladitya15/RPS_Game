d9827da612a164c2253a2c6f97d9f2ba4bda27e4
khanaladitya15 <53925332+khanaladitya15@users.noreply.github.com>
2022-03-21 09:11:19 +0545

    playRound function created to play single round of RPS game

--- test.js
+++ test.js
@@ -1,7 +1,30 @@
 // This is a JavaScript code for Rock,Paper and Scissor game !
-function computerPlay(){
-    let arr = ['Rock','Paper','Scissors']
+
+function computerPlay(){ // This function generates random computer action for the game
+    let arr = ['ROCK','PAPER','SCISSORS']
     let returnedArr = arr[Math.floor(Math.random() * arr.length)];
     return returnedArr;
 }
-console.log(computerPlay());
+// This function is used to play single round of RPS Game.
+function playRound(playerSelection,computerSelection){
+    // Condition 1
+    if(playerSelection === ("ROCK"||"rock"||"Rock") && computerSelection === ("SCISSORS"||"scissors"||"Scissors")){
+        console.log("You win! Rock beats Scissors");
+    }else if(playerSelection === ("SCISSORS"||"scissors"||"Scissors") && computerSelection === ("ROCK"||"rock"||"Rock")){
+            console.log('You loose! Rock beats Scissors');  
+    // Condition 2          
+    }else if(playerSelection === ("PAPER"||"paper"||"Paper") && computerSelection === ("SCISSORS"||"scissors"||"Scissors")){
+        console.log('You loose! Scissors beats Paper');
+    }else if(playerSelection === ("SCISSORS"||"scissors"||"Scissors") && computerSelection === ("PAPER"||"paper"||"Paper")){
+        console.log('You win! Scissors beats Paper');
+    //Condition 3    
+    }else if(playerSelection === ("PAPER"||"paper"||"Paper") && computerSelection === ("ROCK"||"rock"||"Rock")){
+        console.log('You win! Paper beats Rock');
+    }
+    else if(playerSelection === ("ROCK"||"rock"||"Rock") && computerSelection === ("PAPER"||"paper"||"Paper")){
+    console.log('You loose! Paper beats Rock');
+    }
+}
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+console.log(playRound());
