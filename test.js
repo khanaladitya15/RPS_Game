f355749702557070527d2a7662214db8c18091b7
khanaladitya15 <53925332+khanaladitya15@users.noreply.github.com>
2022-03-21 18:38:39 +0545

    game function created to loop the program

--- test.js
+++ test.js
@@ -8,26 +8,37 @@ function computerPlay(){ // This function generates random computer action for t
 // This function is used to play single round of RPS Game.
 function playRound(playerSelection,computerSelection){
     // Condition 1
-    if(playerSelection === ("ROCK"||"rock"||"Rock") && computerSelection === ("SCISSORS"||"scissors"||"Scissors")){
+    if(playerSelection === "ROCK" && computerSelection === "SCISSORS"){
         console.log("You win! Rock beats Scissors");
-    }else if(playerSelection === ("SCISSORS"||"scissors"||"Scissors") && computerSelection === ("ROCK"||"rock"||"Rock")){
+    }else if(playerSelection === "SCISSORS" && computerSelection === "ROCK"){
             console.log('You loose! Rock beats Scissors');  
     // Condition 2          
-    }else if(playerSelection === ("PAPER"||"paper"||"Paper") && computerSelection === ("SCISSORS"||"scissors"||"Scissors")){
+    }else if(playerSelection === "PAPER" && computerSelection === "SCISSORS"){
         console.log('You loose! Scissors beats Paper');
-    }else if(playerSelection === ("SCISSORS"||"scissors"||"Scissors") && computerSelection === ("PAPER"||"paper"||"Paper")){
+    }else if(playerSelection === "SCISSORS" && computerSelection === "PAPER"){
         console.log('You win! Scissors beats Paper');
     //Condition 3    
-    }else if(playerSelection === ("PAPER"||"paper"||"Paper") && computerSelection === ("ROCK"||"rock"||"Rock")){
+    }else if(playerSelection === "PAPER" && computerSelection === "ROCK"){
         console.log('You win! Paper beats Rock');
     }
-    else if(playerSelection === ("ROCK"||"rock"||"Rock") && computerSelection === ("PAPER"||"paper"||"Paper")){
+    else if(playerSelection === "ROCK" && computerSelection === "PAPER"){
     console.log('You loose! Paper beats Rock');
-    // else condition
+    }
+    else if(playerSelection != "ROCK" && playerSelection != "PAPER" && playerSelection != "SCISSORS"){
+        console.log('Wrong Input')
     }else{
         console.log('Tie Breaker!');
     }
 }
-const playerSelection = "rock";
+const userPlay = prompt("choose : rock, paper or scissors")
+const playerSelection =  userPlay.toUpperCase()
+console.log(playerSelection)
 const computerSelection = computerPlay();
-console.log(playRound(playerSelection,computerSelection));
+console.log(computerSelection)
+
+function game(){
+    for(let i = 0;i < 5;i++){
+       console.log(playRound(playerSelection,computerSelection));
+    }
+}
+game();
